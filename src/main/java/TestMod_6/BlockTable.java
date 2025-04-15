30f8dd8b9d868e10664dfb3e97e9e12e707b639d
Alexey Sheleg <alex138728@gmail.com>
2025-04-15 19:26:56 +0300

    Added new Block.

---
+++ src/main/java/TestMod_6/BlockTable.java
@@ -0,0 +1,12 @@
+package TestMod_6;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTable extends Block {
+
+	protected BlockTable(Material material) {
+		super(material);
+	}
+
+}
